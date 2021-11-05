546725e818e30e0fcc0ec0527f3e81f38461a9ec
Lesnik56 <lesnikrus5@yandex.ru>
2021-11-05 17:34:54 +0500

    JokeCatalogSO(+-) JokeSO

---
+++ Assets/Recources/ScriptableObject/JokeCatalogSO.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joke : ScriptableObject
+{
+    private List<JokeSO> jokeList;
+
+    public JokeSO[] ReturnThreeRandomJoke()
+    {
+        JokeSO[] threeRandomJoke = new JokeSO[3];
+
+        int randomOne, randomTwo, randomThree;
+
+        randomOne = Random.Range(0, jokeList.Count);
+
+        threeRandomJoke[0] = jokeList[randomOne];
+        do
+        {
+            randomTwo = Random.Range(0, jokeList.Count);
+        } while (randomOne == randomTwo);
+        threeRandomJoke[1] = jokeList[randomTwo];
+
+        do
+        {
+            randomThree = Random.Range(0, jokeList.Count);
+        } while (randomTwo == randomThree && randomOne == randomThree);
+        threeRandomJoke[2] = jokeList[randomThree];
+
+
+        return threeRandomJoke;
+
+    }
+}
---
+++ Assets/Recources/ScriptableObject/JokeSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu]
+public class JokeSO : ScriptableObject
+{
+  private Result result;
+    private string text;
+    private string forPublic;
+
+    public Result GetResult { get => result;}
+}
